80f4c6ffebd13ce5aafd1881e1d4725cdfb45b13
Henry Stern <stern@fsi.io>
2014-04-01 13:59:01 -0300

    nmsg: Documentation

--- nmsg.py
+++ nmsg.py
@@ -12,6 +12,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Python extension module for the nmsg C library.
+
+A Python extension module implemented in Cython for the nmsg C
+library.
+
+Read some messages::
+
+    import nmsg
+
+    in = nmsg.input.open_file('input.nmsg')
+    while True:
+        m = in.read()
+        if not m:
+            break
+        # do something with m
+
+Write a base.encode message::
+
+    import json
+    import time
+
+    import nmsg
+
+    m = nmsg.msgtype.base.encode()
+    t = time.time()
+    m.time_sec = int(t)
+    m.time_nsec = int((t - int(t)) * 1E9)
+    m.has_source = True
+    m.source = 0x1a1a1a1a # replace with your source id
+
+    m['type'] = 'JSON'
+    m['payload'] = json.dumps({ 'foo' : 'bar' })
+
+    out = nmsg.output.open_file('output.nmsg')
+    out.write(m)
+
+Print all available message types::
+
+    for vname in nmsg.msgtype.types:
+        for mname in nmsg.msgtype.types[vname]:
+            print '.'.join((vname, mname))
+"""
+
 import ctypes
 import sys
 
